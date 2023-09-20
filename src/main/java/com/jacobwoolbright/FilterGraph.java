618ad46967fcc25931fcec4314618b8871f85158
JLW1808 <jacob.woolbright1234@gmail.com>
2023-09-19 22:12:27 -0500

    add custom graph to dryer

--- src/main/java/com/jacobwoolbright/FilterGraph.java
+++ src/main/java/com/jacobwoolbright/FilterGraph.java
@@ -31,11 +31,33 @@ public class FilterGraph {
         return result;
     }
 
-    public static Map<Date, Integer> getGraphPointsWasher(){
+    public static Map<Date, Integer> getGraphPointsDryer(String timespan){
+
         Map<Date, Integer> result = new HashMap<>();
 
-        Map<Date, Integer> data = DatabaseManager.getInstance().getWasherAvailabilityRaw();
+        Map<Date, Integer> data = DatabaseManager.getInstance().getDryerAvailabilityRaw(timespan);
 
+        for(Date apiDate : data.keySet()){
+            boolean found = false;
+            for(Date resultDate : result.keySet()){
+                if(Math.abs(TimeUtils.getDateDiff(apiDate, resultDate, TimeUnit.SECONDS)) <= 120){
+                    result.replace(resultDate, result.get(resultDate) + data.get(apiDate));
+                    found = true;
+                }
+            }
+            if(!found){
+                result.put(apiDate, data.get(apiDate));
+            }
+        }
+
+        return result;
+    }
+
+    public static Map<Date, Integer> getGraphPointsWasher(){
+
+        Map<Date, Integer> result = new HashMap<>();
+
+        Map<Date, Integer> data = DatabaseManager.getInstance().getDryerAvailabilityRaw();
 
         for(Date apiDate : data.keySet()){
             boolean found = false;
@@ -80,6 +102,37 @@ public class FilterGraph {
         Map<Date, Integer> result = new HashMap<>();
 
         Map<Date, Integer> data = DatabaseManager.getInstance().getWasherAvailabilityRaw(timespan);
+
+        int groupTimeInt = Integer.valueOf(groupTime.substring(0, groupTime.length()-1));
+
+        for(Date apiDate : data.keySet()){
+            boolean found = false;
+            for(Date resultDate : result.keySet()){
+                if(groupTime.endsWith("m")){
+                    if(Math.abs(TimeUtils.getDateDiff(apiDate, resultDate, TimeUnit.MINUTES)) <= groupTimeInt){
+                        result.replace(resultDate, result.get(resultDate) + data.get(apiDate));
+                        found = true;
+                    }
+                } else if (groupTime.endsWith("h")) {
+                    if(Math.abs(TimeUtils.getDateDiff(apiDate, resultDate, TimeUnit.HOURS)) <= groupTimeInt){
+                        result.replace(resultDate, result.get(resultDate) + data.get(apiDate));
+                        found = true;
+                    }
+                }
+            }
+            if(!found){
+                result.put(apiDate, data.get(apiDate));
+            }
+        }
+
+        return result;
+    }
+
+    public static Map<Date, Integer> getGraphPointsDryer(String timespan, String groupTime){
+
+        Map<Date, Integer> result = new HashMap<>();
+
+        Map<Date, Integer> data = DatabaseManager.getInstance().getDryerAvailabilityRaw(timespan);
 
         int groupTimeInt = Integer.valueOf(groupTime.substring(0, groupTime.length()-1));
 
