610952f485ffb97efaa32b16f9ac8f7b09d43ece
JLW1808 <jacob.woolbright1234@gmail.com>
2023-09-24 13:14:35 -0500

    create Availability class

---
+++ src/main/java/com/jacobwoolbright/Availability.java
@@ -0,0 +1,13 @@
+package com.jacobwoolbright;
+
+public class Availability {
+    private java.util.Date time;
+    private Integer available;
+
+    public Availability(java.util.Date time, Integer available) {
+        this.time = time;
+        this.available = available;
+    }
+
+    // getters and setters
+}
